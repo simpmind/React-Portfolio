6c82f47481445a74327113598327ea3062ed38b2
Huy Dang <huydang@huys-mbp.mynetworksettings.com>
2022-11-11 01:10:49 -0500

    update again

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-// import GithubIcon from "@material-ui/icons/GitHub";
-// import TwitterIcon from "@material-ui/icons/Twitter";
-// import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GithubIcon from "@material-ui/icons/GitHub";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
 
 function Footer() {
@@ -9,13 +9,13 @@ function Footer() {
     <div className="footer">
       <div className="socialMedia">
         <a href="https://www.github.com/simpmind/">
-          {/* <GithubIcon /> */}
+          <GithubIcon />
         </a>
         <a href="https://twitter.com/elonmusk">
-          {/* <TwitterIcon /> */}
+          <TwitterIcon />
         </a>
         <a href="https://www.linkedin.com/">
-          {/* <LinkedInIcon /> */}
+          <LinkedInIcon />
         </a>
       </div>
       <p> &copy; 2022 DUC DANG</p>
