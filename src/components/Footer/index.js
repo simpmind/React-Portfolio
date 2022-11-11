83af4e129e2e4521d29b2966d8d2ab245b40b1f1
Huy Dang <huydang@huys-mbp.mynetworksettings.com>
2022-11-11 00:51:30 -0500

    lastchance

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,22 +1,21 @@
 import React from "react";
+// import GithubIcon from "@material-ui/icons/GitHub";
+// import TwitterIcon from "@material-ui/icons/Twitter";
+// import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
-
-import { VscGithubAlt } from "react-icons/vsc";
-import { AiOutlineLinkedin } from "react-icons/ai";
-import { FaInstagram } from "react-icons/fa";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia">
         <a href="https://www.github.com/simpmind/">
-          <VscGithubAlt />
+          {/* <GithubIcon /> */}
         </a>
-        <a href="https://instagram.com/elonmusk">
-          <FaInstagram />
+        <a href="https://twitter.com/elonmusk">
+          {/* <TwitterIcon /> */}
         </a>
         <a href="https://www.linkedin.com/">
-          <AiOutlineLinkedin />
+          {/* <LinkedInIcon /> */}
         </a>
       </div>
       <p> &copy; 2022 DUC DANG</p>
